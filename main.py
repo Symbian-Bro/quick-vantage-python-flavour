9e9a0cb8535333bf538a43fb94c6237a31a811d8
Symbian-Bro <siddhubhai998@gmail.com>
2025-07-18 20:53:42 +0530

    Created the main file in which all the functions receives their input from.

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ def main():
     while True:
         status()
         choice = input("\nChoose an option (1-3, q): ").lower()
+
+        if choice == '1':
+            toggle_conservation()
+        elif choice == '2':
+            toggle_fn_lock()
+        elif choice == 'q':
+            print("Goodbye!")
+            break # See ya later
+        else:
+            print("Invalid option, please try again")
+        time.sleep(1)  # I'll be back soon
+
+if __name__ == "__main__":
+    main()
