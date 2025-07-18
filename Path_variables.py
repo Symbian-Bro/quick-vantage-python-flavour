0976f4d14b3dccdc89969c569b78c1f320ca82a9
Symbian-Bro <siddhubhai998@gmail.com>
2025-07-18 05:31:26 +0530

    Added the index [0] to the glob function in the path variables declaration file. We only want the first match in the list of matches (If there are :) )

--- Path_variables.py
+++ Path_variables.py
@@ -1,3 +1,3 @@
 # Will need this is a seperate function because I am new to using glob instead of wildcards
 def find_path(pattern):# The pattern is the path to the file we are looking for
-    return glob.glob(pattern)
+    return glob.glob(pattern)[0]# 0 is the first match. That's what we are looking for
